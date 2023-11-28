82fd3b779ba38010af00e94475c79eec17eec3bf
faizulhassanRien <faizulhassan.rien@gmail.com>
2023-11-28 14:44:38 +0600

    fixed components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import BluePrint from './component/BluePrint/BluePrint'
+import GetUpdate from './component/GetUpdate/GetUpdate'
+import IndustryTrends from './component/IndustryTrends/IndustryTrends'
+import Integration from './component/Integration/Integration'
+import ProductVideo from './component/ProductVideo/ProductVideo'
+import Retouched from './component/Retouched/Retouched'
+import TheSquad from './component/TheSquad/TheSquad'
+import Footer from './component/footer/footer'
 import Navbar from './component/navber/navbar'
+import Record from './component/record/record'
+import WelcomePage from './component/welcomePage/welcomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <Navbar/>
+      <ProductVideo/>
+      <WelcomePage/>
+      <Record/>
+      <Retouched/>
+      <Integration/>
+      <TheSquad/>
+      <BluePrint/>
+      <IndustryTrends/>
+      <GetUpdate/>
+      <Footer/>
     </>
   )
 }
---
+++ src/component/record/circleStyle.jsx
@@ -0,0 +1,29 @@
+
+
+const CircleStyle = () => {
+    return (
+        <div className='flex flex-col items-center'>
+            <div>
+                <div className='p-6 bg-kow-green rounded-full z-20 relative'>
+                    <div className='rounded-full'>
+                        <div className='shadow-2xl w-[130px] h-[130px] flex bg-[#f9f9f9] rounded-full items-center justify-center'>
+                            <h5 className='bg-white rounded-full text-4xl font-semibold'>12M</h5>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-col items-center justify-center relative top-[42px] z-10 gap-3'>
+                    <div className='absolute -top-[70px] bg-[#666666] w-[1px] h-[70px]'></div>
+                    <div className='p-[7px] border-[1px] border-solid border-[#666666] rounded-full'>
+                        <div className='bg-kow-green p-2 rounded-full'></div>
+                    </div>
+                    <div>
+                    <h5 className='uppercase text-lg font-medium'>2d Assets</h5>
+            </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default CircleStyle;
