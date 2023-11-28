7f6da864ca5f90c8b73b4a4d175769779ee53e2d
faizulhassanRien <faizulhassan.rien@gmail.com>
2023-11-28 18:30:25 +0600

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 import { Route, Routes } from 'react-router-dom'
 import HomeContainer from './component/HomeContainer/HomeContainer'
+import BlogPage from './component/BlogPage/BlogPage'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomeContainer />} />
+        <Route path="/industry-trends" element={<BlogPage />} />
       </Routes>
       {/* <Navbar />
       <div className="bg-[#DEE2E2] py-3 flex flex-col">
