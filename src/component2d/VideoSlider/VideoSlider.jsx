1cc47363e0a5b0c59e4d21613173907fcc197bda
shafayat18 <shafayatplay@gmail.com>
2023-12-13 12:30:37 +0600

    solved some issues

--- src/component2d/VideoSlider/VideoSlider.jsx
+++ src/component2d/VideoSlider/VideoSlider.jsx
@@ -23,6 +23,21 @@ const VideoSlider = () => {
             "thumb": "/img/video_slider/video-slider-3.jpg",
             "video": "/img/sl-3.jpg",
             "label": "Background removal"
+        },
+        {
+            "thumb": "/img/video_slider/video-slider-1.jpg",
+            "video": "/img/sl.jpg",
+            "label": "Photo recolor"
+        },
+        {
+            "thumb": "/img/video_slider/video-slider-2.jpg",
+            "video": "/img/sl-2.jpg",
+            "label": "Background removal"
+        },
+        {
+            "thumb": "/img/video_slider/video-slider-3.jpg",
+            "video": "/img/sl-3.jpg",
+            "label": "Background removal"
         }
     ]
 
@@ -48,24 +63,6 @@ const VideoSlider = () => {
                     >
                         {sliderList.map((item, index) => (
                             <VideoWithThumb index={index} video={video} thumb={item.thumb} label={item.label} />
-                            // <div className='flex flex-col mx-1 gap-2'>
-
-                            //     <div className="flex justify-center w-full md:w-[768px] absolute top-0 opacity-0 transition-all duration-300" style={{ opacity: playBool ? 1 : 0 }}>
-                            //         <video ref={videoRef}  width="100%" height="auto" controls muted loop>
-                            //             <source src={video} type="video/mp4" />
-                            //         </video>
-                            //     </div>
-
-                            //     <div onClick={playVideo} style={{ opacity: playBool ? 0 : 1 }} key={index} className='z-10 float-left relative h-full cursor-pointer'>
-                            //         <img className='w-full h-full object-cover' src={item.thumb} alt="slider" />
-                            //         <div className='absolute bottom-2 left-0 w-full flex justify-center' style={{ height: 'auto' }}>
-                            //             <div className='text-xl flex gap-3 text-white items-center'>
-                            //                 <span>{item.label}</span>
-                            //                 <img src='/img/video_slider/play-icon.svg' className='w-[20px] h-[20px]' alt="slider" />
-                            //             </div>
-                            //         </div>
-                            //     </div>
-                            // </div>
                         ))}
                     </Carousel>
                 </div>
