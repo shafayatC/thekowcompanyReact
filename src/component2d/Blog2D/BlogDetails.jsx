a1bd2f07d998d37613224e93fb8aacc02143e18f
faizulhassanRien <faizulhassan.rien@gmail.com>
2023-12-15 15:06:55 +0600

    updated

--- src/component2d/Blog2D/BlogDetails.jsx
+++ src/component2d/Blog2D/BlogDetails.jsx
@@ -1,10 +1,13 @@
-import Page1 from "./Page1";
+import Page2 from "./Page2";
+import Page3 from "./Page3";
+
 
 
 const BlogDetails = () => {
     return (
         <div >
-           <Page1/>
+            <Page2/>
+           <Page3/>
         </div>
     );
 };
