8285daf4040140a60cc2cdf3ce73f0fcc8798c23
faizulhassanRien <faizulhassan.rien@gmail.com>
2023-12-15 15:36:55 +0600

    updated

--- src/component2d/Blog2D/BlogDetails.jsx
+++ src/component2d/Blog2D/BlogDetails.jsx
@@ -1,13 +1,15 @@
 import Page2 from "./Page2";
 import Page3 from "./Page3";
+import Page4 from "./Page4";
 
 
 
 const BlogDetails = () => {
     return (
         <div >
-            <Page2/>
+            {/* <Page2/> */}
            <Page3/>
+           <Page4/>
         </div>
     );
 };
---
+++ src/component2d/Blog2D/Page4.jsx
@@ -0,0 +1,28 @@
+import blogImg1 from "../../../public/img/Blog-Images/blogPhoto1.png"
+import blogImg2 from "../../../public/img/Blog-Images/blogPhoto2.png"
+const Page4 = () => {
+    return (
+        <div className="bg-[#DEE2E2]">
+            <div className="py-20">
+                <div className="flex flex-col md:flex-row justify-center items-center gap-2">
+                    <div>
+                        <img src={blogImg1}/>
+                    </div>
+                    <div>
+                        <img src={blogImg2}/>
+                    </div>
+                </div>
+                <div className="container mx-auto py-5">
+                    <p className="text-justify">
+                    Other than that, you can focus on lifestyle photography where you take photos with a relevant background and natural lighting. Also, pairing
+up your products and shooting them together can aid your business by reducing the need for props and result in duel sales. Try to pick
+a relevant setup, for example- photographing wines near a vineyard or grape garden, cars on the road, men and women’s formal wear near
+a tall corporate building etc. This is likely to help the viewer better imagine the product’s use in their lives.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Page4;
