42bb909e6d49ebdcc0bc564d404e0bf8498aa255
faizulhassanRien <faizulhassan.rien@gmail.com>
2024-01-04 17:57:01 +0600

    update

--- src/component3d/Navbar3D/Navbar3D.jsx
+++ src/component3d/Navbar3D/Navbar3D.jsx
@@ -6,12 +6,21 @@ import { useEffect, useRef, useState } from "react";
 import { HashLink } from "react-router-hash-link";
 import { RiArrowDownSFill } from "react-icons/ri";
 import { IoMdArrowDropdown } from "react-icons/io";
+import MegaMenu from "../MegaMenu/MegaMenu";
 
 const Navbar3D = () => {
     const [getMenuBool, setMenuBool] = useState(false);
     const menuRef = useRef(null);
 
+   
 
+  
+      const [showDiv, setShowDiv] = useState(false);
+    
+      const toggleDiv = () => {
+        setShowDiv(!showDiv);
+      };
+    
     // useEffect(()=>{
 
 
@@ -38,7 +47,16 @@ const Navbar3D = () => {
         <>
             {/* <div className="bg-[#DEE2E2] font-sans w-full min-h-screen m-0"> */}
             <div ref={menuRef} className="fixed w-full top-0 z-[999]  shadow-xl md:shadow-none bg-[length:100%_100%]">
+            {showDiv && <div 
+                             className="absolute w-full  "
+                             style={{ top: 'calc(100% + 5px)' }}
+                            >
+                                
+                                <MegaMenu/>
+                                
+                                </div>}
                 <div className="container mx-auto px-4 md:px-0 py-1">
+                    
                     <div className="hidden md:flex items-center justify-between gap-3">
                         <Link to={"/"}>
                             <img src={logo}
@@ -48,12 +66,17 @@ const Navbar3D = () => {
                         </Link>
                         {/* --------------------------------------For Large Screen------------------------------------------ */}
                         {/* <div className="hidden md:flex sm:items-center md:gap-1 lg:gap-8 xl:gap-12"> */}
-                        <div className="flex gap-24 items-center">
+                        <div className="flex gap-24 items-center relative">
 
-                            <button  className="text-gray-800 text-xl lg:text-sm font-medium flex gap-1 items-center">SERVICES
+                            
+                            <button onClick={toggleDiv}  className="text-gray-800 text-xl lg:text-sm font-medium flex gap-1 items-center">SERVICES
                             {/* <span><RiArrowDownSFill /></span> */}
                             <span><IoMdArrowDropdown/></span>
+                            
                             </button>
+                           
+                      
+                            
                             <HashLink to="/2d#apps" className="text-gray-800 text-xl lg:text-sm font-medium">3D TRENDS</HashLink>
                             <HashLink to="/2d#price" className="text-gray-800 text-xl lg:text-sm font-medium">CONTACT US</HashLink>
                             <HashLink to="/2d#2d-contact" className="text-gray-800 text-xl lg:text-sm font-medium">CONTACT US</HashLink>
