89600c99a52033e8b102d4fd56432eacf038c638
shafayat18 <shafayatplay@gmail.com>
2024-01-08 14:25:51 +0600

    updated

--- src/component3d/Navbar3D/Navbar3D.jsx
+++ src/component3d/Navbar3D/Navbar3D.jsx
@@ -46,7 +46,7 @@ const Navbar3D = () => {
     return (
         <>
             {/* <div className="bg-[#DEE2E2] font-sans w-full min-h-screen m-0"> */}
-            <div ref={menuRef} className={`fixed w-full top-0 z-[999]  shadow-xl md:shadow-none  bg-[length:100%_100%] ${showDiv && `bg-[url("/img/3d/bg_3.jpg")] h-[310px]`}`}>
+            <div ref={menuRef} className={`fixed w-full top-0 z-[999]  shadow-xl md:shadow-none  bg-[length:100%_100%] ${showDiv && `bg-[url("/img/3d/bg_3.jpg")] h-[310px]`}  ${getMenuBool && `bg-[url("/img/3d/bg_3.jpg")]`}`}>
 
                 <div className="container mx-auto px-4 md:px-0 py-1">
 
@@ -84,32 +84,33 @@ const Navbar3D = () => {
                             <Link to="/" className="text-gray-800 text-[26px] lg:text-[32px]font-medium"><GoHomeFill />
                             </Link>
                         </div>
-                        <div className="flex items-center justify-between md:hidden">
-                            <Link to={"/"}>
-                                <img src={logo}
-                                    width={80}
-                                    height={40}
-                                    alt="logo" />
-                            </Link>
-                            <CgMenuGridO onClick={() => setMenuBool(!getMenuBool)} className="text-[32px]  cursor-pointer" />
+                    </div>
 
-                        </div>
+                    <div className={`flex items-center justify-between md:hidden`}>
+                        <Link to={"/"}>
+                            <img src={logo}
+                                width={80}
+                                height={40}
+                                alt="logo" />
+                        </Link>
+                        <CgMenuGridO onClick={() => setMenuBool(!getMenuBool)} className="text-[32px]  cursor-pointer" />
+
                     </div>
                 </div>
                 {/* -----------------------------------For Mobile--------------------------------------- */}
                 {
-                    getMenuBool && <div className="block md:hidden bg-[#DEE2E2] pt-2 ">
+                    getMenuBool && <div className="block md:hidden pt-2 ">
                         <div className="container mx-auto">
                             <div className="flex flex-col text-center gap-4">
-                                <HashLink onClick={() => setMenuBool(false)} to="/2d#services" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#DEE2E2] font-semibold py-3 hover:bg-[#cfd6d6] hover:border-b-2 hover:border-[#7C9C30]">SERVICES</HashLink>
-                                <HashLink onClick={() => setMenuBool(false)} to="/2d#apps" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#DEE2E2] font-semibold py-3 hover:bg-[#cfd6d6] hover:border-b-2 hover:border-[#7C9C30]">APPS</HashLink>
-                                <HashLink onClick={() => setMenuBool(false)} to="/2d#price" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#DEE2E2] font-semibold py-3 hover:bg-[#cfd6d6] hover:border-b-2 hover:border-[#7C9C30]">PRICE</HashLink>
-                                <HashLink onClick={() => setMenuBool(false)} to="/2d#order-guide" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#DEE2E2] font-semibold py-3 hover:bg-[#cfd6d6] hover:border-b-2 hover:border-[#7C9C30]">ORDER GUIDE</HashLink>
-                                <HashLink onClick={() => setMenuBool(false)} to="/2d#2d-trends" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#DEE2E2] font-semibold py-3 hover:bg-[#cfd6d6] hover:border-b-2 hover:border-[#7C9C30]">2D TRENDS</HashLink>
-                                <HashLink onClick={() => setMenuBool(false)} to="/2d#2d-contact" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#DEE2E2] font-semibold py-3 hover:bg-[#cfd6d6] hover:border-b-2 hover:border-[#7C9C30]">CONTACT US</HashLink>
+                                <HashLink onClick={() => setMenuBool(false)} to="/2d#services" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#376f50] font-semibold py-3 hover:bg-[#376f50] hover:border-b-2 hover:border-[#7C9C30] hover:text-white">SERVICES</HashLink>
+                                <HashLink onClick={() => setMenuBool(false)} to="/2d#apps" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#376f50] font-semibold py-3 hover:bg-[#376f50] hover:border-b-2 hover:border-[#7C9C30] hover:text-white">APPS</HashLink>
+                                <HashLink onClick={() => setMenuBool(false)} to="/2d#price" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#376f50] font-semibold py-3 hover:bg-[#376f50] hover:border-b-2 hover:border-[#7C9C30] hover:text-white">PRICE</HashLink>
+                                <HashLink onClick={() => setMenuBool(false)} to="/2d#order-guide" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#376f50] font-semibold py-3 hover:bg-[#376f50] hover:border-b-2 hover:border-[#7C9C30] hover:text-white">ORDER GUIDE</HashLink>
+                                <HashLink onClick={() => setMenuBool(false)} to="/2d#2d-trends" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#376f50] font-semibold py-3 hover:bg-[#376f50] hover:border-b-2 hover:border-[#7C9C30] hover:text-white">2D TRENDS</HashLink>
+                                <HashLink onClick={() => setMenuBool(false)} to="/2d#2d-contact" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#376f50] font-semibold py-3 hover:bg-[#376f50] hover:border-b-2 hover:border-[#7C9C30] hover:text-white">CONTACT US</HashLink>
 
-                                <a onClick={() => setMenuBool(false)} href="https://app.cutoutwiz.com/Identity/Account/Login?ReturnUrl=%2F" target="_blank" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#DEE2E2] font-semibold py-3 hover:bg-[#cfd6d6] hover:border-b-2 hover:border-[#7C9C30]" rel="noreferrer">SIGN UP</a>
-                                <Link onClick={() => setMenuBool(false)} to="/" className="transition-all duration-300 flex flex-col items-center text-gray-800 text-[32px] border-b-2 py-3 hover:bg-[#cfd6d6] border-[#DEE2E2] font-semibold hover:border-b-2 hover:border-[#7C9C30]"><GoHomeFill /></Link>
+                                <a onClick={() => setMenuBool(false)} href="https://app.cutoutwiz.com/Identity/Account/Login?ReturnUrl=%2F" target="_blank" className="transition-all duration-300 text-gray-800 text-base border-b-2 border-[#376f50] font-semibold py-3 hover:bg-[#376f50] hover:border-b-2 hover:border-[#7C9C30] hover:text-white" rel="noreferrer">SIGN UP</a>
+                                <Link onClick={() => setMenuBool(false)} to="/" className="transition-all duration-300 flex flex-col items-center text-gray-800 text-[32px] border-b-2 py-3 hover:bg-[#376f50] border-[#376f50] font-semibold hover:border-b-2 hover:border-[#7C9C30] hover:text-white"><GoHomeFill /></Link>
                             </div>
                         </div>
                     </div>
