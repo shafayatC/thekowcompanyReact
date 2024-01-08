0dbb0a3249541a2da57300ecd8f3a0a983ff90d0
shafayat18 <shafayatplay@gmail.com>
2024-01-08 14:49:32 +0600

    updated

--- src/component3d/Services3D/ServiceList.jsx
+++ src/component3d/Services3D/ServiceList.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ServiceList = ({ icon, title, description, divider = false }) => {
     return (
-        <div className='flex items-end justify-center gap-32'>
-           {divider && <div className='w-[2px] md:h-20 bg-gradient-to-t from-[#4f9e75]  to-[#cddcc8]'></div>} 
+        <div className='flex flex-col md:flex-row items-center md:items-end justify-center gap-7 md:gap-32'>
+           {divider && <div className='w-[90%] md:w-[2px] h-[2px] md:h-20 bg-gradient-to-t from-[#4f9e75]  to-[#cddcc8]'></div>} 
             <div className='flex flex-col justify-center items-center '>
                 <div>
                     <img src={icon} alt='service icon' />
@@ -13,7 +13,7 @@ const ServiceList = ({ icon, title, description, divider = false }) => {
                     <p className='w-[150px] text-center'>{description}</p>
                 </div>
             </div>
-            {divider && <div className='w-[2px] md:h-20 bg-gradient-to-t from-[#4f9e75]  to-[#cddcc8]'></div> }
+            {divider && <div className='w-[90%] md:w-[2px] h-[2px] md:h-20 bg-gradient-to-t from-[#4f9e75]  to-[#cddcc8]'></div> }
         </div>
     );
 };
