9771816aa23023c4be6c8502bd044c60a4720e4d
faizulhassanRien <faizulhassan.rien@gmail.com>
2024-01-09 16:35:22 +0600

    update

--- src/component3d/ExteriorVisualization/ExteriorVisualization.jsx
+++ src/component3d/ExteriorVisualization/ExteriorVisualization.jsx
@@ -3,12 +3,18 @@ import ScrolTop from '../../component/ScrolTop/ScrolTop';
 import Navbar3D from '../Navbar3D/Navbar3D';
 import Contact3D from '../Contact3D/Contact3D';
 import Footer3D from '../Footer3D/Footer3D';
+import EVPage2 from './EVPage2/EVPage2';
+import EVPage3 from './EVPage3/EVPage3';
+import EVPage4 from './EVPage4/EVPage4';
 
 const ExteriorVisualization = () => {
     return (
         <div>
             <ScrolTop />
             <Navbar3D />
+            <EVPage2/>
+            <EVPage3/>
+            <EVPage4/>
             <Contact3D />
             <Footer3D />
         </div>
