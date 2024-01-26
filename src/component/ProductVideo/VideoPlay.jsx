a9d922856a99d879adea8576de01672108b968d0
shafayat18 <shafayatplay@gmail.com>
2024-01-26 14:21:48 +0600

    updated

--- src/component/ProductVideo/VideoPlay.jsx
+++ src/component/ProductVideo/VideoPlay.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const VideoPlay = () => {
+
+    const [getVideo, setVideo] = useState('/video/The Kow Company_video.mp4'); 
+    // const videoLoadFunc = async () => {
+    //     try {
+    //         const response = await fetch('/video/The Kow Company_video.mp4');
+    //         const blob = await response.blob();
+    //         setVideo(URL.createObjectURL(blob));
+    //         console.log(getVideo);
+    //     } catch (error) {
+            
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     videoLoadFunc(); 
+    // },[])
     return (
         <div className="flex justify-center w-full h-full">
         <video className="object-cover" width="100%" height="auto" playsinline autoPlay muted loop>
-            <source src='/video/The Kow Company_video.mp4' type="video/mp4" />
+            <source src={getVideo} type="video/mp4" />
         </video>
     </div>
     );
--- src/component3d/ProductVisualization3d/Furniture/Model/ChairModel.jsx
+++ src/component3d/ProductVisualization3d/Furniture/Model/ChairModel.jsx
@@ -8,6 +8,7 @@ const ChairModel = () => {
   const { nodes, materials } = useGLTF('/chair.glb')
   // Cursor showing current color
   const [hovered, set] = useState(null);
+  const [cursorIs, setCursorIs] = useState(false);
   const [colors, setColors] = useState({});
   const [pickColor, setPickColor] = useState("#aabbcc");
   const [getModelObject, setModelObject] = useState(null);
@@ -24,8 +25,8 @@ const ChairModel = () => {
             key={index}
             geometry={geometry}
             material={materials[material.name]}
-            onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name), console.log(color))}
-            onPointerOut={(e) => e.intersections.length === 0 && set(null)}
+            onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name), setCursorIs(true), console.log(color))}
+            onPointerOut={(e) => ( setCursorIs(false), e.intersections.length === 0 && set(null))}
             // onClick={(event) => (console.log(event), event.object.material.color.set(0xce2020))}
             // onClick={(event) => (console.log(event), renderColors(event))}
             onClick={(event) => (console.log(event), setModelObject(event))}
@@ -62,7 +63,7 @@ const ChairModel = () => {
   useEffect(() => {
     const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${colors[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="${colors[hovered]}" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`;
     const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`;
-    if (hovered) {
+    if (cursorIs) {
       document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(
         cursor
       )}'), auto`;
